d6124432fa279c8ed44e9ed00ebab4cfcd79eaca
עדן חסן <edenhasan@gmail.com>
2019-01-18 16:53:48 +0200

    init

--- config/keys.js
+++ config/keys.js
@@ -3,4 +3,9 @@
 // } else {
 
 // }
-module.exports = require('./keys_dev');
+// module.exports = require('./keys_dev');
+
+module.exports = {
+  mongoURI: 'mongodb://edenhasan:Eden6600@ds223578.mlab.com:23578/gamestrade',
+  secret: 'secret'
+};
